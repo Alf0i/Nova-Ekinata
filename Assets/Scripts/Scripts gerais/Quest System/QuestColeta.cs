63e3bd8143b2dd0dbedf48685eede045fa49196f
Alf0i <iagoalfaya@gmail.com>
2023-07-17 17:25:19 -0300

    quest funciona (incompleta)

--- Assets/Scripts/Scripts gerais/Quest System/QuestColeta.cs
+++ Assets/Scripts/Scripts gerais/Quest System/QuestColeta.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuestColeta : ObjetivoSM
+public class QuestColeta : MonoBehaviour, ObjetivoSM
 {
     public bool AtualizarObjetivo(Transform[] objs)
     {
